205f375b26b9d6632ea526a39ea0318468676efb
Nageeb Zafar <75119519+ZafarNageeb@users.noreply.github.com>
2021-10-31 19:43:19 +0100

    List all Items in user Downloads folder in console

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import os
+
+def get_download_path():
+    if os.name == 'nt':
+        import winreg
+        sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
+        downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
+            location = winreg.QueryValueEx(key, downloads_guid)[0]
+        return location
+    else:
+        return os.path.join(os.path.expanduser('~'), 'downloads')
 
 def main():
-    print("test")
+    files = [f for f in os.listdir(get_download_path()) ]
+    for f in files:
+        print(f)
 
 if __name__ == '__main__':
     main()
-
