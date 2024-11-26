e61275f74809c85fd264b509bcce894c07401003
Nageeb Zafar <nageebahmad3000@gmail.com>
2024-11-26 15:03:42 +0100

    Adjusted Code to work on all platforms and to sort the files correctly

--- DownloadSorter.py
+++ DownloadSorter.py
@@ -1,40 +1,56 @@
 import os
 import shutil
-from glob import glob
 
 
 def get_download_path():
-    if os.name == 'nt':
+    """Determine the user's Downloads folder based on the operating system."""
+    if os.name == 'nt':  # Windows
         import winreg
         sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
         downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
         with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
-            location = winreg.QueryValueEx(key, downloads_guid)[0]
-        return location
+            return winreg.QueryValueEx(key, downloads_guid)[0]
+    elif os.name == 'posix':  # macOS or Linux
+        return os.path.join(os.path.expanduser('~'), 'Downloads')
     else:
-        return os.path.join(os.path.expanduser('~'), 'downloads')
+        raise OSError("Unsupported operating system")
 
 
 def main():
+    # Get the path to the Downloads folder
     downloads_path = get_download_path()
-    files = [f for f in os.listdir(downloads_path)]
-    folder_list = glob(downloads_path + "/*/")
-    for f in files:
-        print(f)
-        name, extension = os.path.splitext(f)
+
+    if not os.path.exists(downloads_path):
+        print(f"Downloads folder not found: {downloads_path}")
+        return
+
+    # Get all files in the Downloads folder
+    files = [f for f in os.listdir(downloads_path) if os.path.isfile(os.path.join(downloads_path, f))]
+
+    for file in files:
+        # Extract file name and extension
+        name, extension = os.path.splitext(file)
+
+        if not extension:  # Skip files without an extension
+            continue
+
+        # Create folder name based on file extension (without the dot)
+        folder_name = extension[1:]
+
+        # Path for the folder to move files into
+        destination_folder = os.path.join(downloads_path, folder_name)
+
         try:
-            if extension[1:] in folder_list:
-                shutil.move(downloads_path + "/" + f, downloads_path + "/" + extension[1:])
-                print("Moved file {" + f + "} ")
-            else:
-                to_create_directory = downloads_path + "/" + extension[1:]
-                os.makedirs(to_create_directory)
-                print("Created Folder {" + extension[1:] + "} ")
-                folder_list.append(extension[1:])
-                shutil.move(downloads_path + "/" + f, downloads_path + "/" + extension[1:])
-                print("Moved file {" + f + "} to " + extension[1:] + " folder")
-        except:
-            files.remove(f)
+            # Create the folder if it doesn't exist
+            if not os.path.exists(destination_folder):
+                os.makedirs(destination_folder)
+                print(f"Created folder: {folder_name}")
+
+            # Move the file into the folder
+            shutil.move(os.path.join(downloads_path, file), os.path.join(destination_folder, file))
+            print(f"Moved file: {file} to {folder_name}/")
+        except Exception as e:
+            print(f"Error processing file {file}: {e}")
 
 
 if __name__ == '__main__':
